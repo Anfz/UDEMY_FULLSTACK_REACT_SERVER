203e9418a49a4a75201552f6e6305a86db9444dc
Chris Beaver <chrisbvr@gmail.com>
2017-08-31 22:24:01 +1000

    step one of google oauth flow

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ passport.use(
   })
 );
 
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']  //ask google to give access to...
+  })
+);
+
 //heroku environment variables
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
