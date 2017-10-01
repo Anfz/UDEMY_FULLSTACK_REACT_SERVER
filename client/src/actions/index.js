fca02156be86d8b016a20f15b032200ec068db6d
Chris Beaver <chrisbvr@gmail.com>
2017-10-01 21:14:32 +1000

    refactored authReducer to return false if there is no payload for fetchUser

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 import {FETCH_USER} from './types';
 
 export const fetchUser = () => async (dispatch) => {
-    const currentUser = await axios.get('/api/current_user')
-    dispatch({type: FETCH_USER, payload: currentUser});
+    const currentUserResponse = await axios.get('/api/current_user')
+    dispatch({type: FETCH_USER, payload: currentUserResponse.data});
 };
