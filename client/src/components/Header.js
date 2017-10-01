5d5ebf3163394c3aa69a9ba5fcb3d9e36fc17bd9
Chris Beaver <chrisbvr@gmail.com>
2017-10-01 21:30:20 +1000

    added user state to header component - login/logout will display on the right hand side of the nav bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,35 @@
 import React, {Component} from 'react'; 
+import {connect} from 'react-redux';
 
 class Header extends Component {
+    renderContent(){
+      switch(this.props.auth){
+          case null: 
+            return null;
+          case false: 
+            return (<li><a href="/auth/google">Login with google</a></li>);
+          default:
+            return (<li><a href="/api/logout">Logout</a></li>);
+      }
+    }
 
-  render() {
-      return (
+
+    render() {
+        return (
         <nav>
             <div className="nav-wrapper">
-                <a className="brand-logo">Emaily</a>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a>Login With Google</a></li>
+                <a className="left brand-logo">Emaily</a>
+                <ul className="right">
+                    {this.renderContent()}
                 </ul>
             </div>
         </nav>
-      );
-  }
+        );
+    }
 }
 
-export default Header; 
+function mapStateToProps(state){
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header); 
