f8088f27702e8e0645f65909428db5e808060327
Chris Beaver <chrisbvr@gmail.com>
2017-10-01 19:34:43 +1000

    setup of survey and landing page routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,19 @@
 import React from 'react'; 
 import { BrowserRouter, Route} from 'react-router-dom'; 
 
-
 const Header = () => <h2>Header</h2>;
 const DashBoard = () => <h2>DashBoard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
-
 
 const App = () => {
     return (
         <div>
           <BrowserRouter>
             <div>
-                <Route path="/" component={Landing} />
+                <Route exact={true} path="/" component={Landing} />
+                <Route exact path="/surveys" component={DashBoard} />
+                <Route exact path="/surveys/new" component={SurveyNew} />
             </div>
           </BrowserRouter>  
         </div>
