0beb039242dab18bc9b56f0f20ba89af4fa072e4
Chris Beaver <chrisbvr@gmail.com>
2017-10-02 17:42:59 +1000

    added handle token action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,13 @@ import axios from 'axios';
 import {FETCH_USER} from './types';
 
 export const fetchUser = () => async (dispatch) => {
-    const currentUserResponse = await axios.get('/api/current_user')
+    const currentUserResponse = await axios.get('/api/current_user');
+    
     dispatch({type: FETCH_USER, payload: currentUserResponse.data});
 };
+
+export const handleToken = (token) => async dispatch => {
+    const userResponse = await axios.post('/api/stripe', token);
+
+    dispatch({type: FETCH_USER, payload: userResponse.data});
+}
