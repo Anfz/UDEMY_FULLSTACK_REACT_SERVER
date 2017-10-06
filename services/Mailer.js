35b6e696c188e8bb99cb51cbdde72d3d394b8707
Chris Beaver <chrisbvr@gmail.com>
2017-10-07 09:12:21 +1000

    setup email properties

--- services/Mailer.js
+++ services/Mailer.js
@@ -4,7 +4,23 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+  // restricts the first element will need to have subject and recipients field (kinda like a interface)
+  constructor({ subject, recipients}, content) {
+    super(); 
 
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients); 
+  }
+
+  formatAddresses (recipients){
+    return recipients.map( ({email}) =>{
+      return new helper.Email(email);
+    });
+  }
+
+  
 }
 
 
