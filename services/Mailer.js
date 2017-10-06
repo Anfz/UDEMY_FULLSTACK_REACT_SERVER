08f423c3dac73d783ff6a210284dcb437fd194df
Chris Beaver <chrisbvr@gmail.com>
2017-10-07 09:29:30 +1000

    finished mailer, tutorial guy hates sendgrid

--- services/Mailer.js
+++ services/Mailer.js
@@ -7,11 +7,16 @@ class Mailer extends helper.Mail {
   // restricts the first element will need to have subject and recipients field (kinda like a interface)
   constructor({ subject, recipients}, content) {
     super(); 
-
+    this.sgApi = sendgrid(keys.sendGridKey); 
     this.from_email = new helper.Email('no-reply@emaily.com');
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients); 
+
+    this.addContent(this.body);
+    this.addClickTracking(); 
+    this.addRecipients(); 
+
   }
 
   formatAddresses (recipients){
@@ -20,7 +25,35 @@ class Mailer extends helper.Mail {
     });
   }
 
-  
+  addClickTracking(){
+    const trackingSettings = new helper.TrackingSettings(); 
+    const clickTracking = new helper.ClickTracking(true, true); 
+
+    trackingSettings.setClickTracking(clickTracking); 
+    this.addTrackingSettings(trackingSettings); 
+  }
+
+  addRecipients(){
+    const personalize = new helper.Personalization(); 
+
+    this.recipients.forEach(recipipient => {
+      personalize.addTo(recipient); 
+    }); 
+
+    this.addPersonalization(personalize); 
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST', 
+      path: '/v3/mail/send', 
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request); 
+    return response; 
+  }
+
 }
 
 
