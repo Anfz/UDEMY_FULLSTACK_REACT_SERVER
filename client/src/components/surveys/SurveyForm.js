285f19df4730aef76d1a0da1774ee0ef864330a3
Chris Beaver <chrisbvr@gmail.com>
2017-10-08 20:24:02 +1000

    Submit Form boilerplate

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
-import {reduxForm} from 'redux-form';
+import {reduxForm, Field} from 'redux-form';
 class SurveyForm extends Component {
   render() {
     return( 
       <div>
-        Survey Form
+        <form 
+        onSubmit={this.props.handleSubmit(values => console.log(values))}
+          >       
+          <Field 
+            type="text"
+            name="surveyTitle"
+            component="input"
+          />
+        </form>
+
       </div>
     );
   }
