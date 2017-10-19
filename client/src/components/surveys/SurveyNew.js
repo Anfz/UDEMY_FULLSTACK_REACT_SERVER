7e8580e91ca5f1e9db54a082b9dd50a67e7259da
Chris Beaver <chrisbvr@gmail.com>
2017-10-19 19:54:06 +1000

    survey form/review shown on survey new

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 class SurveyNew extends Component {
+  
+  /*constructor(props){
+    super(props);
+    this.state = {new: true}
+  }*/
+  //babel allows this (from create-react-app)
+  state = { showFormReview: false };
+
+  renderContent(){
+    if (this.state.showFormReview === true){
+      return <SurveyFormReview></SurveyFormReview>;
+    }
+    return <SurveyForm></SurveyForm>;
+  }
   render() {
     return( 
       <div>
-        <SurveyForm></SurveyForm>
+       {this.renderContent()}
       </div>
     );
   }
