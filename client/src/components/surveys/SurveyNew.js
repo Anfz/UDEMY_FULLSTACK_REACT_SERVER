a277a5b6c459de0346d9d58237f1454fa9816112
Chris Beaver <chrisbvr@gmail.com>
2017-10-24 09:20:22 +1000

    survey review back button

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -12,7 +12,8 @@ class SurveyNew extends Component {
 
   renderContent(){
     if (this.state.showFormReview === true){
-      return <SurveyFormReview></SurveyFormReview>;
+      return <SurveyFormReview
+      onCancel={() => this.setState({showFormReview:false})}></SurveyFormReview>;
     }
     return <SurveyForm 
                onSurveySubmit={() => this.setState({ showFormReview: true })}>
